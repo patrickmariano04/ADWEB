d4dffc18973be0c979207151d6d81b337f28e2eb
patrickmariano04 <99706405+patrickmariano04@users.noreply.github.com>
2023-02-04 11:49:12 +0800

    10 files modified, 1 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { AchievementsComponent } from './achievements/achievements.component';
@@ -8,17 +10,18 @@ import { ReferencesComponent } from './references/references.component';
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
 
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'achievements', component: AchievementsComponent },
+  { path: 'references', component: ReferencesComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'skills', component: SkillsComponent },
+];
+//Step 5 - use RouterModule
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    AchievementsComponent,
-    ReferencesComponent,
-    EducationComponent,
-    SkillsComponent,
-  ],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [RouterModule.forRoot(routes), BrowserModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
